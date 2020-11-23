55d7ca6e37c1f3acd8c3d107cae4fdd8206cb95e
shravankb <shravan.kumar@nineleaps.com>
2020-11-23 13:28:38 +0530

    [basic folder setup] feature | Folder structuring done

---
+++ src/api/user/user-route.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const userController = require('./userController');
+
+router.post('/', )
+
+router.get('/')
+
+router.get('/')
+
+router.post('/')
+
---
+++ src/api/user/userService.js
@@ -0,0 +1,1 @@
+const userDAL = require('./userDAL');
